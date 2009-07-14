68890147c518807a94170ef74cb94ac13c494607
paul.orrock <paul.orrock@d77a72c5-9a25-0410-8410-7b13b68b695a>
2009-07-14 20:44:42 +0000

    Changed throttling code to work as per Betfair docs, based on clock minute not 60 seconds

--- trunk/lib/BetFair/Throttle.pm
+++ trunk/lib/BetFair/Throttle.pm
@@ -6,13 +6,23 @@ BetFair::Throttle
 
 =head1 SYNOPSIS
 
-Betfair API limits the number of requests made by a client application per minute, eg. you 
-can only login 24 times in 60 seconds.
+Betfair API limits the number of requests made by a client application per 
+clock minute, eg. you can only login 24 times in each minute. However you can
+login 24 times in the last 5 seconds of a minute and 24 times in the first 
+5 seconds of the next minute.
 
-BetFair::Throttle helps manage API calls made by your client by throttling the requests.
+BetFair::Throttle helps manage API calls made by your client by throttling 
+the requests.
 
 That is to say, Throttle will record the time of every request, and prevent 
-futher calls if the number of requests exceeds the alotted total.
+further calls if the number of requests exceeds the allotted total.
+
+=head1 NOTES
+
+It is critically important that your machine is synced to several valid NTP
+timesources since the counter is reset every minute at Betfair, your machine 
+needs to be as close to this as possible so that you don't get throttled when
+you think you're ok and vice versa.
 
 =head1 AUTHOR
 
@@ -24,7 +34,7 @@ introduce sleeper method, to wait until it's ok to continue program flow
 
 move the %throttle data structure to a config file 
 
-need to make the touch & count values persistant, ie. for multi-threaded programs to share 
+need to make the touch & count values persistent, ie. for multi-threaded programs to share 
 
 =cut
 
@@ -40,15 +50,11 @@ use BetFair::Trace qw( TRACE );
 my $PACKAGE = 'BetFair::Throttle';
 my $VERSION = '';
 
-my $cycle = 60; # seconds
-
-# TODO 
-#  - introduce sleeper method, to wait until it's ok to continue program flow
-#  - move the %throttle data structure to a config file 
-#  - need to make the touch & count values persistant, ie. for multi-threaded programs to share 
+my $conf = new BetFair::Config;
+my $cycle = $conf->{cycle} ? $conf->{cycle} : '60';
 
 # template data structure for holding information about the number of API requests
-# allowed every minutes.
+# allowed every minute.
 my %throttle = 
 	(
 	 'login' => { 'limit' => 24, 'call' => 0, 'count' => 0 },
@@ -62,64 +68,55 @@ my %throttle =
 	);
 
 
-sub new
-{
-        my $class = shift;
-        my $objref = \%throttle;
-        bless $objref, $class;
+sub new {
+    my $class = shift;
+    my $objref = \%throttle;
+    bless $objref, $class;
 	TRACE("$PACKAGE : created a new instance of throttle",1);
-        return $objref;
+    return $objref;
 }
 
-
-# record number of times method is called in a 60 second cycle
-sub touch
-{
+# Increment call to $method by 1
+sub touch {
 	my ( $self, $method ) = @_;
-
-	# if the last call is less than a minute ago increment count
-	my $t = time();
-	$t =~ /([0-9]+)\./; 
-		
-	# difference in seconds between first call & now
-	my $diff = $1 - $self->{$method}{call};
-	TRACE("$PACKAGE->touch : difference between first call and now is '$diff' seconds", 1); 	
-
-	# count the number of calls made in a cycle
-	if ( $diff < $cycle )
-		{
-                 $self->{$method}{count}++;
-		 TRACE("$PACKAGE->touch : incrementing method '$method' count to '".$self->{$method}{count}."'", 1);
-		}
-	else 
-		{ 
-		TRACE("$PACKAGE->touch : resetting method '$method' count to 0", 1);
-		# reset count to 1, then log the time of this call
-		$self->{$method}{count} = 1;
-		$self->{$method}{call} = $1;
-		}		
-		
-	return 1;
+    if ( ! $self->{$method}->{call} ) {
+       $self->{$method}->{call} = int((time-1)/60); 
+    }
+    $self->{$method}{count}++;
+    return 1;
 }
 
+=item 
+
+The ok_to_call method needs to work out how many times the method has been 
+called in the last minute and do all the cleaning up so it can be confident 
+whether we can be called again. 
+
+=cut
 
 sub ok_to_call
 {
 	my ( $self, $method ) = @_;
 
-	my $c = $self->{$method}->{count};
-	my $l = $self->{$method}->{limit};
-
-	TRACE("$PACKAGE->ok_to_call : count = '$c', limit = '$l'", 1);
-	
-	if ( $c > $l ) 
-	{
-	 TRACE("$PACKAGE->ok_to_call : Throttle limit exceeded on '$method'", 1);
-	 return 0;
+    # Fast as you like, user has turned off throttling
+    return 1 if ($cycle == 1);
+       
+    # Would calling $method again push us over what we think the method is ?
+	if ( ($self->{$method}->{count}+1) > $self->{$method}->{limit} ) {
+        #full minutes since epoch, 1 second slow for safety
+        my $thisminute = int((time-1)/60); 
+        if ($self->{$method}->{call} < $thisminute ) {
+            $self->{$method}->{call} = $thisminute;
+            $self->{$method}->{count} = 0;
+        } else {
+            TRACE("$PACKAGE->ok_to_call : Throttle limit exceeded on '$method'", 1);
+	        return 0;
+        }
 	}
 
 	# otherwise ok  
-	TRACE("$PACKAGE->ok_to_call : Throttle limit ok on '$method'", 1);
+    print "$PACKAGE->ok_to_call : Throttle limit ok on '$method' (".$self->{$method}->{count}." / ".$self->{$method}->{limit} .")";
+	TRACE("$PACKAGE->ok_to_call : Throttle limit ok on '$method' (".$self->{$method}->{count}." / ".$self->{$method}->{limit} .")", 1);
 	return 1;
 }
 
