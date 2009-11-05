d3af4de87a437c759cf668f915c2b23964adc77e
paul.orrock <paul.orrock@d77a72c5-9a25-0410-8410-7b13b68b695a>
2009-11-05 22:54:38 +0000

    Formatting changes in Betfair::Request

--- lib/BetFair/Request.pm
+++ lib/BetFair/Request.pm
@@ -35,24 +35,24 @@ my $VERSION = '';
 my $conf = new BetFair::Config;
 my $logfile = $conf->{log} ? $conf->{log} . 'request.log' : '';
 
-# All calls whose type is below have to go to the global api, everything else 
+# All calls whose type is below have to go to the global api, everything else
 # should go to the exchange server. See Betfair API docs.
 my @globalAPI = qw(addPaymentCard convertCurrency createAccount deletePaymentCard depositFromPaymentCard forgotPassword getActiveEventTypes getAllCurrencies getAllEventTypes getEvents getPaymentCard getSubscriptionInfo keepAlive login logout modifyPassword modifyProfile retrieveLIMBMessage submitLIMBMessage transferFunds updatePaymentCard viewProfile withdrawToPaymentCard );
 
 sub new
 {
-        my $class = $_[0];
-        my $objref = {
-		response => '',
-		message => '',
+    my $class = $_[0];
+    my $objref = {
+	    response => '',
+	    message => '',
         error => '',
-		logfile => $logfile,
+    	logfile => $logfile,
 		type => '',
-	        throttle => new BetFair::Throttle
-                };
-        bless $objref, $class;
+        throttle => new BetFair::Throttle
+    };
+    bless $objref, $class;
 	TRACE("$PACKAGE : new request object created", 1);
-        return $objref;
+    return $objref;
 }
 
 sub request
@@ -69,13 +69,13 @@ sub request
 
  my $userAgent = LWP::UserAgent->new();
  $userAgent->env_proxy;
- 
+
  my $request = HTTP::Request->new( POST => $soap_uri );
 
  $request->header( SOAPAction => $soap_header );
  $request->content($self->{message});
  $request->content_type("text/xml; charset=utf-8");
-  
+
  # reset the error string;
  $self->{error} = '';
 
@@ -85,13 +85,13 @@ sub request
   my $response = $userAgent->request($request);
   $self->{response} = $response->{'_content'};
   $self->{throttle}->touch( $self->{type} );
-  
+
   # log the request/response
-  
+
   if ( $conf->{log} )
   {
       TRACE("$PACKAGE->request log directory '$self->{logfile}' does not exist", 1) unless ( -e $self->{logfile} );
-      
+
       open(LOG, ">>".$self->{logfile}) || TRACE($!);
       print LOG "--- Request ----------\n";
       print LOG $self->{message} . $/;  # TODO - password isn't hidden here & should be.
@@ -100,7 +100,7 @@ sub request
       print LOG "\n\n**********************\n\n";
       close LOG;
   }
-  
+
   TRACE($response->{'_content'}, 2);
 
   # check response is parsable
@@ -110,13 +110,13 @@ sub request
   {
    TRACE("$PACKAGE : Response unparsable, ie. not XML. Is the BetFair service ok? http://service.betfair.info/");
    $self->{error} = "$PACKAGE : Response unparsable, ie. not XML. Is the BetFair service ok? http://service.betfair.info/\n" . $self->{message} . "\n\n" . $self->{response};
-  }	
+  }
  }
  else
  {
   $self->{response} = '';
   $self->{error} = "$PACKAGE : Slow down. Throttle reports limit exceeded for " . $self->{type} . " - request not made.";
-  TRACE("$PACKAGE : Slow down. Throttle reports false, request not made.", 1); 
+  TRACE("$PACKAGE : Slow down. Throttle reports false, request not made.", 1);
  }
 
  return 1;
@@ -127,7 +127,7 @@ sub message
  my ($self, $message, $type) = @_;
  TRACE("$PACKAGE->message : assigning new message of type '$type' to request", 1);
  $self->{message} = $message;
- $self->{type} = $type; 
+ $self->{type} = $type;
  return 1;
 }
 
